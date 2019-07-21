1f83c9d5d4dce0601681c09662402abb6aba6903
louisalemar <louisalemar@gmail.com>
2019-07-21 18:47:24 -0400

    fix: backend favicon error

--- volumes/back_end/index.js
+++ volumes/back_end/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const favicon = require('serve-favicon')
 const port = 8080
+
+app.use(favicon(path.join(__dirname, 'src/assets/images/', 'favicon.ico')))
 
 app.get('/', (req, res) => res.send('Hello World from back-end!'))
 
