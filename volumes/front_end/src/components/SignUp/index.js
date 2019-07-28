4e607cf92f90159dbf4f41b332f54a36a406ad35
louisalemar <louisalemar@gmail.com>
2019-07-27 23:18:22 -0400

    expand register form

--- volumes/front_end/src/components/SignUp/index.js
+++ volumes/front_end/src/components/SignUp/index.js
@@ -5,10 +5,28 @@ class SignUp extends Component{
 		return(
 			<div>
 				<h2>Sign-Up</h2>
-				<form action="">
-					<input type="text"/>
-					<input type="text"/>
-					<button>Submit</button>
+				<form>
+					<div className="container">
+						<h3>Register</h3>
+						<p>Please fill in this form to create an account.</p>
+						<hr />
+
+						<label htmlFor="email"><strong>Email</strong></label>
+						<input type="text" placeholder="Enter Password" name="password" required />
+
+						<label htmlFor="password"><b>Password</b></label>
+						<input type="password" placeholder="Enter Password" name="password" required />
+
+						<label htmlFor="password-repeat"><b>Repeat Password</b></label>
+						<input type="password" placeholder="Repeat Password" name="password-repeat" required />
+						<hr />
+
+						<p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+						<button type="submit" className="registerbtn">Register</button>
+					</div>
+					<div className="container signin">
+						<p>Already have an account? <a href="#">Sign in</a>.</p>
+					</div>
 				</form>
 			</div>
 		)
